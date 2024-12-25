4a5fd75101b11c81e223d1046753f489dc798f7e
Aditi <aditisharma0032@gmail.com>
2024-12-26 03:12:56 +0530

    Added Chat-bot to application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,20 @@ import MyOrders from "./pages/MyOrder/MyOrders";
 const App = () => {
   const [showLogin, setShowLogin] = React.useState(false);
 
+
+  React.useEffect(() => {
+    const loadTawkTo = () => {
+      const script = document.createElement("script");
+      script.async = true;
+      script.src = "https://embed.tawk.to/676c75e0af5bfec1dbe1eae3/1ifvqovji";
+      script.charset = "UTF-8";
+      script.setAttribute("crossorigin", "*");
+      document.body.appendChild(script);
+    };
+
+    loadTawkTo();
+  }, []);
+
   return (
     <>
       {showLogin ? <LoginPopup setShowLogin={setShowLogin}/> : <> </>}
