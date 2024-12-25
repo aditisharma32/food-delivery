e0d51e933f74f2a4297e3ee98661add6fed43e1f
Aditi <aditisharma0032@gmail.com>
2024-12-25 16:07:24 +0530

    Handling Exception in FoodItem.js

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -56,22 +56,36 @@ React.useEffect(() => {
           <p>Image not available</p>
         )}
 
-        {!cartItems[id]
-            ?<img src={assets.add_icon_white} alt="" className='add' onClick={()=>addToCart(id)}/>
-            :<div className='food-item-counter'>
-                <img src={assets.remove_icon_red} alt="" onClick={()=>removeFromCart(id)}/>
-                <p>{cartItems[id]}</p>
-                <img src={assets.add_icon_green} alt="" onClick={()=>addToCart(id)}/>
-              </div>
-        }
+        {!cartItems?.[id] ? (
+          <img
+            src={assets.add_icon_white}
+            alt=""
+            className="add"
+            onClick={() => addToCart?.(id)}
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              src={assets.remove_icon_red}
+              alt=""
+              onClick={() => removeFromCart?.(id)}
+            />
+            <p>{cartItems?.[id]}</p>
+            <img
+              src={assets.add_icon_green}
+              alt=""
+              onClick={() => addToCart?.(id)}
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
-            <p>{name}</p>
-            <img src={assets.rating_starts} alt="" />
+          <p>{name || 'Unnamed Food Item'}</p>
+          <img src={assets.rating_starts} alt="Rating Stars" />
         </div>
-        <p className="food-item-desc">{description}</p>
-        <p className="food-item-price">${price}</p>
+        <p className="food-item-desc">{description || 'No description available'}</p>
+        <p className="food-item-price">${price ?? 'N/A'}</p>
       </div>
     </div>
   )
